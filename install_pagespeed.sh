f2592469e2ead9f2b5e7ad57cefdd2f6eb1a0562
Emanuel Righetto <posta@emanuelrighetto.it>
2018-11-10 01:43:15 +0100

    bugfix nginx

--- install_pagespeed.sh
+++ install_pagespeed.sh
@@ -18,51 +18,21 @@ cd /tmp
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}/
-    ./configure
-        --prefix=/usr/share/nginx
-        --sbin-path=/usr/sbin/nginx
-        --modules-path=/usr/lib/nginx/modules
-        --conf-path=/etc/nginx/nginx.conf
-        --pid-path=/var/run/nginx/nginx.pid
-        --lock-path=/var/run/nginx/nginx.lock
-        --http-client-body-temp-path=/var/cache/nginx/client_temp
-        --http-proxy-temp-path=/var/cache/nginx/proxy_temp
-        --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp
-        --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp
-        --http-scgi-temp-path=/var/cache/nginx/scgi_temp
-        --user=application
-        --group=application
-        --with-compat
-        --with-file-aio
-        --with-http_addition_module
-        --with-http_auth_request_module
-        --with-http_dav_module
-        --with-http_flv_module
-        --with-http_geoip_module
-        --with-http_gunzip_module
-        --with-http_gzip_static_module
-		--with-http_image_filter_module=dynamic
-        --with-http_mp4_module
-        --with-http_random_index_module
-        --with-http_realip_module
-        --with-http_secure_link_module
-		--with-http_slice_module
-        --with-http_ssl_module
-        --with-http_stub_status_module
-        --with-http_sub_module
-        --with-http_v2_module
-		--with-http_xslt_module=dynamic
-        --with-ipv6
-        --with-ld-opt="-Wl,-z,relro,--start-group -lapr-1 -laprutil-1 -licudata -licuuc -lpng -lturbojpeg -ljpeg"
-        --with-mail
-        --with-mail_ssl_module
-        --with-pcre-jit
-        --with-stream
-        --with-stream_ssl_module
-		--with-stream_ssl_preread_module
-		--with-stream_realip_module
-		--with-stream_geoip_module=dynamic
-        --with-threads
+    ./configure \
+        --prefix=/etc/nginx \
+        --sbin-path=/usr/sbin/nginx \
+        --modules-path=/usr/lib/nginx/modules \
+        --conf-path=/etc/nginx/nginx.conf \
+        --pid-path=/var/run/nginx/nginx.pid \
+        --lock-path=/var/run/nginx/nginx.lock \
+        --http-client-body-temp-path=/var/cache/nginx/client_temp \
+        --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+        --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+        --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+        --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+        --user=application \
+        --group=application \
+        --with-http_v2_module \
         --add-module=/tmp/$nps_dir
 make
 make install
